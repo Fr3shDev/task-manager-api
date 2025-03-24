2f77991b2e5f094413095fd2c6b4ae7d61bddafa
Fresh Dev <billyhans90@gmail.com>
2025-03-24 12:18:11 +0100

    Update the main package

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,12 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/Fr3shDev/task-manager-api/internal/handlers"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, Task Manager API!")
 	})
+	// User endpoints.
+	http.HandleFunc("/users/register", handlers.RegisterHandler)
+	http.HandleFunc("/users/login", handlers.LoginHandler)
+
+	// Task endpoints.
+	http.HandleFunc("/tasks", handlers.TasksHandler)
+	http.HandleFunc("/tasks/", handlers.TaskDetailHandler)
 
 	port := "8080"
 	log.Printf("Server is running on port %s\n", port)
